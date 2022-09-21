5f274af2223fb404c02a3a211df7ed43b80b07e0
martucichi16 <mcichi24@ucema.edu.ar>
2022-09-21 08:18:53 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  const nombre = "Juan";
+  const apellido = "Perez";
+
+  console.log(nombre, apellido);
+  console.log("Hola " + nombre + " " + apellido)
+
+  let edad = 40;
+  console.log(edad)
+
+  edad = 30
+  console.log(edad)
+
+  const anios = 20
+  console.log(anios + 31)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Const - let- var</h1>
+    </>
   );
 }
 
 export default App;
+
+// No se suele usar mucho el var, sino que se usa let //
+// Repositorio nuevo de fundamentosjs //
